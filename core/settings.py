5a09405c2bb59b7750839aee7fe2e4727e6b5941
Vashuev <rohitbst.104764@gmail.com>
2025-01-08 14:05:51 +0530

    database updated

--- core/settings.py
+++ core/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webstimulate_cms',                      
+        'USER': 'postgres',
+        'PASSWORD': 'POST$user#12',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
